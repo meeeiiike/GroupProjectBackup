92edc2e68ad5f366b96a6c8303640255d5e5f8f4
meeeiiike <g00407095@atu.ie>
2025-03-13 20:47:41 +0000

    test

--- src/main/java/ie/atu/GroupProjectBackup/Main.java
+++ src/main/java/ie/atu/GroupProjectBackup/Main.java
@@ -1,4 +1,4 @@
-package ie.atu.GroupProject;
+package ie.atu.GroupProjectBackup;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/ie/atu/GroupProjectBackup/TestConnection.java
+++ src/main/java/ie/atu/GroupProjectBackup/TestConnection.java
@@ -1,4 +1,4 @@
-package ie.atu.GroupProject;
+package ie.atu.GroupProjectBackup;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
