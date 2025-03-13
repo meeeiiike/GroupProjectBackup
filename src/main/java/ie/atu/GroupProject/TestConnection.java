7d3e2d99685458d1851a8ea2bce60c6a1a7e8987
meeeiiike <g00407095@atu.ie>
2025-03-13 13:49:10 +0000

    added dependencies & Test connection class which successfully connects to our DB

---
+++ src/main/java/ie/atu/GroupProject/TestConnection.java
@@ -0,0 +1,26 @@
+package ie.atu.GroupProject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sms", "root", "password");
+            System.out.println("Connected");
+        } catch(SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        } finally {
+            if(con != null){
+                try{
+                    con.close();
+                } catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
